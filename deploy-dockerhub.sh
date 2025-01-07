4eebed94f88ff409edb9d9074439fc9f61231c8f
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-01-07 17:05:12 +1100

    chore: docker deploy script for latest-fat

--- deploy-dockerhub.sh
+++ deploy-dockerhub.sh
@@ -5,28 +5,36 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-VERSION_TAG=$1
+HUB="nktnet/s-pdf"
+VERSION_TAG="$1-fat"
+LATEST_TAG="latest-fat"
 
 echo "Building Docker image for version ${VERSION_TAG}..."
-docker build --build-arg VERSION_TAG=${VERSION_TAG} -t nktnet/s-pdf:${VERSION_TAG} -t nktnet/s-pdf:latest .
+docker build \
+    --no-cache \
+    --pull \
+    --build-arg VERSION_TAG="${VERSION_TAG}" \
+    -t "${HUB}:${VERSION_TAG}" \
+    -t "${HUB}:${LATEST_TAG}" \
+    -f ./Dockerfile.fat .
+
 if [ $? -ne 0 ]; then
   echo "Error: Docker build failed."
   exit 1
 fi
 
 echo "Pushing Docker image with version ${VERSION_TAG}..."
-docker push nktnet/s-pdf:${VERSION_TAG}
+docker push "${HUB}:${VERSION_TAG}"
 if [ $? -ne 0 ]; then
   echo "Error: Docker push failed for ${VERSION_TAG}."
   exit 1
 fi
 
-echo "Pushing Docker image with latest tag..."
-docker push nktnet/s-pdf:latest
+echo "Pushing Docker image with ${LATEST_TAG} tag..."
+docker push "${HUB}:${LATEST_TAG}"
 if [ $? -ne 0 ]; then
-  echo "Error: Docker push failed for latest."
+  echo "Error: Docker push failed for ${LATEST_TAG}."
   exit 1
 fi
 
-echo "Docker image with version ${VERSION_TAG} and latest tag successfully pushed!"
-
+echo "Docker image with version ${VERSION_TAG} and ${LATEST_TAG} tag successfully pushed!"
